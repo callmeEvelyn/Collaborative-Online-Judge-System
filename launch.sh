29a85cb42ae14176fcbd3481ff1499b40d9c2c7d
Shulin Huang <shulin.huang0301@gmail.com>
2018-03-03 16:11:03 -0500

    readme

--- launch.sh
+++ launch.sh
@@ -1,21 +1,8 @@
-#!/usr/bin/env bash
-fuser -k 3000/tcp
-fuser -k 5000/tcp
+# npm install -g nodemon
+cd ./oj-client
+# npm install
+ng build --watch &&
 
-service redis_6379 start
-cd ./oj-server/
-npm install
-nodemon server.js &
-cd ././oj-client/
-npm install
-ng build --watch &
-cd ././executor/
-pip install -r requirement.txt
-python executor-server.py &
-
-echo "===================================="
-read -p "PRESS [ENTER] TO TERMINATE PROCESSES." PRESSKEY
-
-fuser -k 3000/tcp
-fuser -k 5000/tcp
-service redis_6379 stop
+cd ../oj-server
+# npm install
+nodemon server.js
